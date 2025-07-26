8d3003d24df8da5e12ca8d897713c9f5301295eb
Nabil Douss <34912372+nabildouss@users.noreply.github.com>
2025-07-26 09:54:42 +0200

    Time: 3 ms (92.99%) | Memory: 42.7 MB (99.85%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cs
+++ 567-permutation-in-string/permutation-in-string.cs
@@ -1,28 +1,32 @@
 public class Solution {
     public bool CheckInclusion(string s1, string s2) {
         if (s1.Length > s2.Length) return false;
-        var dict = new Dictionary<char, int>();
+        var freqs1 = new int[26];
+        var freqs2 = new int[26];
+
         foreach(var c in s1) {
-            if(dict.Keys.Contains(c)) dict[c]++;
-            else dict[c] = 1;
+            freqs1[c - 'a']++;
         }
-        var dict2 = new Dictionary<char, int>(dict);
         var i = 0;
         var j = 0;
-        while (i < s2.Length && j < s2.Length){
-            var c = s2[j];
-            if(!dict.Keys.Contains(c) || !dict2.Keys.Contains(c)) {
-                dict2 = new Dictionary<char, int>(dict);
-                i++;
-                j = i;
-            }
-            else {
-                dict2[c]--;
-                j++;
-                if(dict2[c] == 0) dict2.Remove(c);
-                if(dict2.Keys.Count() == 0) return true;
-            }
+        while ( j < s2.Length && j - i + 1 < s1.Length ){
+            freqs2[s2[j] - 'a']++;
+            j++;
+        }
+        while (j < s2.Length) {
+            freqs2[s2[j]-'a']++;
+            if(AreEqual(freqs1, freqs2)) return true;
+            freqs2[s2[i] - 'a']--;
+            i++;
+            j++;
         }
         return false;
+    }
+
+    public bool AreEqual(int[] a, int[] b) {
+        for(int i=0; i < 26; i++) {
+            if(a[i] != b[i]) return false;
+        }
+        return true;
     }
 }
