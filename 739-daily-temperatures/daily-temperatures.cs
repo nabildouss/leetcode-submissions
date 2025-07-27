fa537398d2549fac88350f4fcd41f701ac21234a
Nabil Douss <34912372+nabildouss@users.noreply.github.com>
2025-07-27 21:57:21 +0200

    Time: 146 ms (5.09%) | Memory: 67.6 MB (73.14%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cs
+++ 739-daily-temperatures/daily-temperatures.cs
@@ -4,13 +4,12 @@ public class Solution {
         var results = new int[n];
         var stack = new Stack<int>();
         
-        for(int i = n-1; i >= 0; i--) {
-            while(stack.Count() > 0 && temperatures[stack.Peek()] <= temperatures[i]) {
-                var pop = stack.Pop();
-            }
-            results[i] = stack.Count() == 0 ? 0 : stack.Peek() - i;
+        for (int i = n-1; i>=0; i--) {
+            while(stack.Count() > 0 && temperatures[i] >= temperatures[stack.Peek()]) stack.Pop();
+            results[i] = stack.Count() == 0? 0 : stack.Peek() - i;
             stack.Push(i);
         }
+        
         return results;
 
     }
