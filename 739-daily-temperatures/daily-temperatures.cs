06914a1a09d6e333707448c3ca2646cf621a6749
Nabil Douss <34912372+nabildouss@users.noreply.github.com>
2025-07-27 11:50:13 +0200

    Time: 145 ms (5.05%) | Memory: 67.4 MB (80.14%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cs
+++ 739-daily-temperatures/daily-temperatures.cs
@@ -1,15 +1,10 @@
 public class Solution {
     public int[] DailyTemperatures(int[] temperatures) {
         var n = temperatures.Length;
-        if (n == 0) return new int[0];
-        
         var results = new int[n];
         var stack = new Stack<int>();
-        stack.Push(n-1);
-        results[n-1] = 0;
         
-        for(int i = n-2; i >= 0; i--) {
-            var current = stack.Count();
+        for(int i = n-1; i >= 0; i--) {
             while(stack.Count() > 0 && temperatures[stack.Peek()] <= temperatures[i]) {
                 var pop = stack.Pop();
             }
