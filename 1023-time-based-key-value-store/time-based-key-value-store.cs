d86f8fc492945460e41d09d91dc05de72bfc3e48
Nabil Douss <34912372+nabildouss@users.noreply.github.com>
2025-07-30 21:52:54 +0200

    Time: 271 ms (5.00%) | Memory: 149.1 MB (72.81%) - LeetSync

---
+++ 1023-time-based-key-value-store/time-based-key-value-store.cs
@@ -0,0 +1,35 @@
+public class TimeMap {
+    private Dictionary<string, List<(int, string)>> _dict;
+    public TimeMap() {
+        _dict = new Dictionary<string, List<(int, string)>>();
+    }
+    
+    public void Set(string key, string value, int timestamp) {
+        if (!_dict.ContainsKey(key)) _dict[key] = new List<(int, string)>();
+        _dict[key].Add((timestamp, value));
+    }
+    
+    public string Get(string key, int timestamp) {
+        if (!_dict.ContainsKey(key)) return string.Empty;
+        return Search(_dict[key], timestamp);
+    }
+
+    private string Search(List<(int, string)> list, int target) {
+        Console.WriteLine(target);
+        var n = list.Count;
+        if (n == 0) return string.Empty;
+        var min = 0;
+        var max = n - 1;
+
+        while (min <= max) {
+            var mid = (min + max) / 2;
+            var (first, second) = list[mid];
+            if (first == target) return second;
+            if (first < target) min = mid + 1;
+            else max = mid - 1;
+        }
+        if (max < 0) max = 0; 
+        var (t, res) = list[max];        
+        return t <= target? res: string.Empty;
+    }
+}
